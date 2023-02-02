3bb3592a3af05ffbdfb224f6830febd4c53f329b
kruss84 <sebastiankrusche@rocketmail.com>
2023-02-02 19:10:08 +0100

    test error

--- tests/test2.py
+++ tests/test2.py
@@ -25,4 +25,5 @@ boxes = to_varabile(boxes_data, requires_grad=False, is_cuda=is_cuda)
 box_index = to_varabile(box_index_data, requires_grad=False, is_cuda=is_cuda)
 
 # set transform_fpcoor to False is the crop_and_resize
-print(RoIAlign.apply(image_torch, boxes, box_index, 3, 3, transform_fpcoor=True))
+roi_align =RoIAlign(3, 3, transform_fpcoor=True)
+print(roi_align(image_torch, boxes, box_index))
